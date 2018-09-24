76a23d1bbb2b724560bd0dc41f220e516e3ba8dd
최인성 <choiis1207@gmail.com>
2018-09-24 23:57:13 +0900

    20180925 1759문제만 수정

--- Algorithm/src/BaekJoon1759.java
+++ Algorithm/src/BaekJoon1759.java
@@ -1,55 +1,67 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Arrays;
-import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class BaekJoon1759 {
 
 	private static Scanner sc = null;
+	private static int N;
+	private static int K;
 	private static char[] arr = null;
-
-	private static PriorityQueue<String> heap = new PriorityQueue<>();
-
-	private static void print_arr(char[] set, int size) {
-		String str = "";
-		for (int i = 0; i < size; i++) {
-			str = str + set[i];
-		}
-		heap.add(str);
-	}
-
-	private static void comb(char[] set, int size, int n, int k, int idx) {
-		if (k == 0) {
-			print_arr(set, size);
-			return;
-		} else if (idx == n) {
-			return;
-		} else {
-			set[size] = arr[idx];
-			comb(set, size + 1, n, k - 1, idx + 1);
-			comb(set, size, n, k, idx + 1);
-		}
-	}
 
 	public static void main(String[] args) throws IOException {
 
 		sc = new Scanner(new FileInputStream("baekjoon1759.txt"));
 
-		int K = sc.nextInt();
-		int N = sc.nextInt();
+		K = sc.nextInt();
+		N = sc.nextInt();
 		arr = new char[N];
-
 		for (int i = 0; i < N; i++) {
 			String str = sc.next();
-			arr[i] = str.toCharArray()[0];
+			arr[i] = str.charAt(0);
 		}
-		char[] set = new char[N];
-		comb(set, 0, N, K, 0);
+		Arrays.sort(arr);
 
-		for (int i = 0; i < heap.size(); i++) {
-			System.out.println(heap.poll());
+		char[] set = new char[K];
+		dfs(0, 0, -1, set);
+	}
+
+	private static void dfs(int idx, int depth, int now, char[] set) {
+
+		if (depth == K) {
+			if (min3(set)) {
+				print(set);
+			}
+			return;
+		} else {
+			for (int i = now + 1; i < Math.min(idx + K, N); i++) {
+				set[depth] = arr[i];
+				dfs(idx + 1, depth + 1, i, set);
+			}
 		}
+	}
+
+	private static boolean min3(char[] set) {
+
+		int mo = 0;
+		int ja = 0;
+		for (int i = 0; i < K; i++) {
+			if (set[i] == 'a' || set[i] == 'e' || set[i] == 'i' || set[i] == 'o' || set[i] == 'u') {
+				mo++;
+			} else {
+				ja++;
+			}
+		}
+
+		return (mo >= 1) && (ja >= 2);
+	}
+
+	private static void print(char[] set) {
+		for (int i = 0; i < set.length; i++) {
+			System.out.print(set[i]);
+		}
+		System.out.println();
 	}
 
 }
