27d87f0c1fda9633b96c60317cac1b33c6a3d174
최인성 <choiis1207@gmail.com>
2018-09-25 09:55:25 +0900

    20180925 BaekJoon1342 update

--- Algorithm/src/BaekJoon1342.java
+++ Algorithm/src/BaekJoon1342.java
@@ -1,58 +1,76 @@
-import java.util.Arrays;
-import java.util.HashSet;
+
 import java.util.Scanner;
 
 public class BaekJoon1342 {
 	private static Scanner sc = null;
 
-	private static HashSet<String> hset = null;
+	private static char[] arr;
 
-	private static boolean numberCheck(char[] arr, int length) {
-		if (length == 0) {
-			return true;
-		}
+	private static int cnt = 0;
 
-		for (int i = 1; i < length; i++) {
-			if (arr[i - 1] == arr[i]) {
-				return false;
-			}
-		}
-		return true;
-	}
+	private static int N;
 
-	private static void swap(char[] arr, int i, int j) {
-		char temp = arr[i];
-		arr[i] = arr[j];
-		arr[j] = temp;
-	}
+	private static void dfs(int idx, int depth, int now, char[] set, boolean[] selected) {
 
-	private static void perm(char[] arr, int pivot) {
-		if (!numberCheck(arr, pivot)) {
-			return;
-		}
-
-		if (pivot == arr.length) {
-			hset.add(new String(arr, 0, pivot));
+		if (depth == N) {
+			cnt++;
 			return;
 		} else {
-			for (int i = pivot; i < arr.length; i++) {
-				swap(arr, i, pivot);
-				perm(arr, pivot + 1);
-				swap(arr, i, pivot);
+			for (int i = 0; i < N; i++) {
+				if (depth == 0) {
+					if (!selected[i]) {
+						set[depth] = arr[i];
+						selected[i] = true;
+						dfs(idx + 1, depth + 1, i, set, selected);
+						selected[i] = false;
+					}
+				} else {
+					if (set[depth - 1] != arr[i]) {
+						if (!selected[i]) {
+							set[depth] = arr[i];
+							selected[i] = true;
+							dfs(idx + 1, depth + 1, i, set, selected);
+							selected[i] = false;
+						}
+					}
+				}
 			}
 		}
+	}
 
+	private static int fac(int n) {
+
+		int cnt = 1;
+		for (int i = n; 1 <= i; i--) {
+			cnt *= i;
+		}
+
+		return cnt;
 	}
 
 	public static void main(String[] args) {
 
 		sc = new Scanner(System.in);
 		String str = sc.next();
+		N = str.length();
+		arr = str.toCharArray();
 
-		hset = new HashSet<>();
-		char[] chars = str.toCharArray();
-		Arrays.sort(chars);
-		perm(chars, 0);
-		System.out.println(hset.size());
+		int[] alphabet = new int[26];
+
+		for (int i = 0; i < arr.length; i++) {
+			alphabet[arr[i] - 'a']++;
+		}
+
+		char[] set = new char[N];
+		boolean[] selected = new boolean[N];
+		dfs(0, 0, -1, set, selected);
+
+		for (int i = 0; i < alphabet.length; i++) {
+			if (alphabet[i] > 1) {
+				cnt /= fac(alphabet[i]);
+			}
+		}
+		System.out.println(cnt);
 	}
+
 }
